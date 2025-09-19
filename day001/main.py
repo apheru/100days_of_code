9e27d6d36a326b44fca1a3f9a016156805e84611
Lucas Natanael <pythonic.apheru@gmail.com>
2025-09-19 08:47:52 -0300

    day 001: introdução ao método print()

---
+++ day001/main.py
@@ -0,0 +1,10 @@
+print("Lucas Natanael (Apheru)")
+print("19 de Setembro, 2025")
+print("""Eu estou começando o desafio de 100 dias de Python do Replit!""")
+print("""
+Eu vou me certificar de passar algum tempo todos os dias codificando, por minimo 10 minutos por dia.
+Mesmo nos dias que pareçam mais dificeis, esse é um termo de compromisso, comigo mesmo.
+Eu estarei usando o VSCODE para está criando os meus códigos durante todo o desafio e não darei desculpas para falhar durante o trajeto.
+""")
+print("Estou me sentindo empolgado no momento! 😜")
+print("Você pode acompanhar meu progresso no meu instagram @apherudev e no meu github, em https://github.com/apheru/100days_of_code_replit")
