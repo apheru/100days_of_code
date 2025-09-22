2af8d27329e9144898a04873471b3a38b268c8ea
Lucas Natanael <pythonic.apheru@gmail.com>
2025-09-21 22:40:52 -0300

    day 003: concatenação

---
+++ day003/main.py
@@ -0,0 +1,8 @@
+nome_comida = input("Nome de uma comida: ")
+nome_planta = input("Nome de um tipo de planta: ")
+metodo_cozimento = input("Nome de um método de cozimento: ")
+comida_queimada = input("Que palavra descreve comida queimada? ")
+nome_ferramenta = input("Nome de uma ferramenta: ")
+
+print("MENU")
+print(metodo_cozimento, "de", nome_comida, "com", comida_queimada, nome_planta, "em uma cama de", nome_ferramenta)
