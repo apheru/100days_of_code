fd0aca5f7e9a8e8e31597f2f555e9b622ba8ad54
Lucas Natanael <pythonic.apheru@gmail.com>
2025-09-22 23:41:07 -0300

    day 004: cores no console

---
+++ colors.py
@@ -0,0 +1,8 @@
+print("\033[30m", "Teste: 1, 2, 3!", "\033[0m", "Ok!") # Preto
+print("\033[31m", "Teste: 1, 2, 3!", "\033[0m", "Ok!") # Vermelho
+print("\033[32m", "Teste: 1, 2, 3!", "\033[0m", "Ok!") # Verde
+print("\033[33m", "Teste: 1, 2, 3!", "\033[0m", "Ok!") # Amarelo
+print("\033[34m", "Teste: 1, 2, 3!", "\033[0m", "Ok!") # Azul
+print("\033[35m", "Teste: 1, 2, 3!", "\033[0m", "Ok!") # Roxo
+print("\033[36m", "Teste: 1, 2, 3!", "\033[0m", "Ok!") # Ciano
+print("\033[37m", "Teste: 1, 2, 3!", "\033[0m", "Ok!") # Branco
---
+++ day004/main.py
@@ -0,0 +1,12 @@
+print("=== Simulador de Aventuras 3000 ===")
+print("""Vamos te fazer algumas perguntas 
+e depois criar uma história incrível,
+com VOCÊ como o protagonista!""")
+print()
+nome = input("Seu nome bravo aventureiro: ")
+inimigo = input("Qual seria o nome do seu temivel pior inimigo: ")
+super_poder = input("Qual seu super poder: ")
+print()
+print(f"Nossa história começa quando nosso herói {nome} se aproxima de um castelo \033[30msombrio e ameaçador\033[0m...")
+print(f"De repente, um raio caiu no chão, bem aos pés de {nome}!")
+print(f"\033[31m'Nossa batalha final começa agora! hahaha !\033[0m — grita o terrível {inimigo}, sem perceber que {nome} possui o poder da \033[35m{super_poder}\033[0m... o que significa que a vitória será fácil demais!")
