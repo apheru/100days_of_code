c3c77b9fe582b31864919a0383c05def6c55cf54
Lucas Natanael <pythonic.apheru@gmail.com>
2025-09-24 23:00:42 -0300

    Day 6 - 100 Days of Code: Elif

---
+++ main.py
@@ -0,0 +1,15 @@
+print("MEU SISTEMA DE LOGIN")
+print("====================")
+username = input("Nome de usuário: ").strip().lower()
+password = input("Senha do usuário: ")
+
+if username == "lucas" and password == "Diagonal753#":
+    print("Bom dia, Lucas! Que hoje seja mais um passo firme na sua jornada, com foco e coragem para conquistar seus objetivos!")
+elif username == "karol" and password == "21121993":
+    print("Bom dia, Karol! Que sua luz e carinho tornem este dia ainda mais especial para todos ao seu redor!")
+elif username == "kevin" and password == "Deidar@321":
+    print("Bom dia, Kevin! Que seu dia seja cheio de energia, alegria e grandes descobertas!")
+elif username == "oliver" and password == "euhove":
+    print("Bom dia, Oliver! Que sua curiosidade e sorriso tornem o mundo mais colorido hoje!")
+else:
+    print("Bom dia! Que o seu dia seja repleto de boas surpresas, alegria e motivação para seguir em frente!")
